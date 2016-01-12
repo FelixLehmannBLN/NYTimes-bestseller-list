105f1d0e94acc0a15e18b24c040d8367f2ffa271
Felix Lehmann <felix-lehmann@gmx.de>
2016-01-12 16:29:00 +0100

    added a non working navigation... :)
    something with the routeprovider does not work

--- app.js
+++ app.js
@@ -10,5 +10,17 @@ bestsellerApp.config(function($routeProvider){
       controller: 'listController'
     });
 
+var bestsellerApp = angular.module('bestsellerApp', ['ngRoute', 'ngResource'])
+
+// setting routes
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'listController',
+        templateUrl: 'pages/list.html'
+      });
+     $routeProvider.otherwise({
+       redirectTo: '/'
+     });
 });
 

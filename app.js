8853e8ece360f26960e946e9201dd253f403508c
Felix Lehmann <felix-lehmann@gmx.de>
2016-01-12 16:58:22 +0100

    something broke the app.js file and duplicated code which i now deleted

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 // module
-var bestsellerApp = angular.module('bestsellerApp', ['ngRoute', 'ngResource']);
-
-// setting routes
-bestsellerApp.config(function($routeProvider){
-
-  $routeProvider
-    .when('/', {
-      templateUrl: 'pages/list.html',
-      controller: 'listController'
-    });
-
 var bestsellerApp = angular.module('bestsellerApp', ['ngRoute', 'ngResource'])
 
 // setting routes

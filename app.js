f2f644528fbf5f63fc20e9260c66ef282510d751
Felix Lehmann <felix-lehmann@gmx.de>
2016-01-12 13:04:47 +0100

    iterated over results list in list.html and added sort function to listcontroller

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 var bestsellerApp = angular.module('bestsellerApp', ['ngRoute', 'ngResource']);
 
 // setting routes
-bestsellerApp.config(function ($routeProvider){
+bestsellerApp.config(function($routeProvider){
 
   $routeProvider
-
-  .when('/', {
-    templateUrl: 'pages/list.html',
-    controller: 'listController'
-  })
+    .when('/', {
+      templateUrl: 'pages/list.html',
+      controller: 'listController'
+    });
 
 });
 
--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -1,9 +1,20 @@
 (function() {
   function listController ($resource) {
-  this.listAPI = $resource("http://api.nytimes.com/svc/books/v3/lists/mass-market-paperback.json?api-key=sample-key");
-  this.listResult = this.listAPI.get();
-  console.log(this.listResult);
-}
+    this.listAPI = $resource("http://api.nytimes.com/svc/books/v3/lists/mass-market-paperback.json?callback=books&api-key=sample-key");
+    this.listResult = this.listAPI.get();
+
+    this.sortBy = 'rank';
+    this.reverse = false;
+    this.sort = function(sortProperty) {
+      this.sortBy = sortProperty;
+      this.reverse = !this.reverse;
+    }
+
+    // just to check if resource is loaded
+    console.log(this.listResult);
+  }
+
+  listController.$inject = ['$resource']
   // assign controller
   bestsellerApp.controller('listController', listController);
 
